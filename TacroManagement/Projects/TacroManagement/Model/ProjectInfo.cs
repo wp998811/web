c8fa616beadc70c0e09ad618f0f3df1f10d07611
veil <wp.veil@gmail.com>
2012-07-26 11:38:28 +0800

    王鹏：project尝试修改

---
+++ TacroManagement/Projects/TacroManagement/Model/ProjectInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    class ProjectInfo
+    {
+        private string ProjectNum;
+        private string ProjectName;
+        private int ProjectAdminID;
+        private string ProjectDescription;
+        private string ProjectType;
+        private string BeginTime;
+        private string EndTime;
+
+        public ProjectInfo()
+        {
+
+        }
+
+        public ProjectInfo(string ProjectNum, string ProjectName, int ProjectAdminID, string ProjectDescription
+            , string ProjectType, string BeginTime, string EndTime)
+        {
+            this.ProjectNum = ProjectNum;
+            this.ProjectName = ProjectName;
+            this.ProjectAdminID = ProjectAdminID;
+            this.ProjectDescription = ProjectDescription;
+            this.ProjectType = ProjectType;
+            this.BeginTime = BeginTime;
+            this.EndTime = EndTime;
+        }
+
+
+    }
+}
