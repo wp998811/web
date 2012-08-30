b66ea8f734738396e9777f4933551e7dace65c10
ZeroV <wei.lin.zero@gmail.com>
2012-08-30 22:16:27 +0800

    简单文件上传，以转flash显示
    差更新数据库

---
+++ TacroManagement/WebSites/TacroManagement/App_Code/Office2Pdf.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Core;
+
+/// <summary>
+/// Office2Pdf 将Office文档转化为pdf
+/// </summary>
+public class Office2Pdf
+{
+    public Office2Pdf()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+    }
+    /// <summary>
+    /// Word转换成pdf
+    /// </summary>
+    /// <param name="sourcePath">源文件路径</param>
+    /// <param name="targetPath">目标文件路径</param>
+    /// <returns>true=转换成功</returns>
+    public bool DOCConvertToPDF(string sourcePath, string targetPath)
+    {
+        bool result = false;
+        Word.WdExportFormat exportFormat = Word.WdExportFormat.wdExportFormatPDF;
+        object paramMissing = Type.Missing;
+        Word.ApplicationClass wordApplication = new Word.ApplicationClass();
+        Word.Document wordDocument = null;
+        try
+        {
+            object paramSourceDocPath = sourcePath;
+            string paramExportFilePath = targetPath;
+            Word.WdExportFormat paramExportFormat = exportFormat;
+            bool paramOpenAfterExport = false;
+            Word.WdExportOptimizeFor paramExportOptimizeFor = Word.WdExportOptimizeFor.wdExportOptimizeForPrint;
+            Word.WdExportRange paramExportRange = Word.WdExportRange.wdExportAllDocument;
+            int paramStartPage = 0;
+            int paramEndPage = 0;
+            Word.WdExportItem paramExportItem = Word.WdExportItem.wdExportDocumentContent;
+            bool paramIncludeDocProps = true;
+            bool paramKeepIRM = true;
+            Word.WdExportCreateBookmarks paramCreateBookmarks = Word.WdExportCreateBookmarks.wdExportCreateWordBookmarks;
+            bool paramDocStructureTags = true;
+            bool paramBitmapMissingFonts = true;
+            bool paramUseISO19005_1 = false;
+            wordDocument = wordApplication.Documents.Open(
+                ref paramSourceDocPath, ref paramMissing, ref paramMissing,
+                ref paramMissing, ref paramMissing, ref paramMissing,
+                ref paramMissing, ref paramMissing, ref paramMissing,
+                ref paramMissing, ref paramMissing, ref paramMissing,
+                ref paramMissing, ref paramMissing, ref paramMissing,
+                ref paramMissing);
+            if (wordDocument != null)
+                wordDocument.ExportAsFixedFormat(paramExportFilePath,
+                    paramExportFormat, paramOpenAfterExport,
+                    paramExportOptimizeFor, paramExportRange, paramStartPage,
+                    paramEndPage, paramExportItem, paramIncludeDocProps,
+                    paramKeepIRM, paramCreateBookmarks, paramDocStructureTags,
+                    paramBitmapMissingFonts, paramUseISO19005_1,
+                    ref paramMissing);
+            result = true;
+        }
+        catch
+        {
+            result = false;
+        }
+        finally
+        {
+            if (wordDocument != null)
+            {
+                wordDocument.Close(ref paramMissing, ref paramMissing, ref paramMissing);
+                wordDocument = null;
+            }
+            if (wordApplication != null)
+            {
+                wordApplication.Quit(ref paramMissing, ref paramMissing, ref paramMissing);
+                wordApplication = null;
+            }
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// 把Excel文件转换成PDF格式文件  
+    /// </summary>
+    /// <param name="sourcePath">源文件路径</param>
+    /// <param name="targetPath">目标文件路径</param>
+    /// <returns>true=转换成功</returns>
+    public bool XLSConvertToPDF(string sourcePath, string targetPath)
+    {
+        bool result = false;
+        Excel.XlFixedFormatType targetType = Excel.XlFixedFormatType.xlTypePDF;
+        object missing = Type.Missing;
+        Excel.ApplicationClass application = null;
+        Excel.Workbook workBook = null;
+        try
+        {
+            application = new Excel.ApplicationClass();
+            object target = targetPath;
+            object type = targetType;
+            workBook = application.Workbooks.Open(sourcePath, missing, missing, missing, missing, missing,
+                missing, missing, missing, missing, missing, missing, missing, missing, missing);
+            workBook.ExportAsFixedFormat(targetType, target, Excel.XlFixedFormatQuality.xlQualityStandard, true, false, missing, missing, missing, missing);
+            result = true;
+        }
+        catch
+        {
+            result = false;
+        }
+        finally
+        {
+            if (workBook != null)
+            {
+                workBook.Close(true, missing, missing);
+                workBook = null;
+            }
+            if (application != null)
+            {
+                application.Quit();
+                application = null;
+            }
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+        return result;
+    }
+    ///<summary>        
+    /// 把PowerPoint文件转换成PDF格式文件       
+    ///</summary>        
+    ///<param name="sourcePath">源文件路径</param>     
+    ///<param name="targetPath">目标文件路径</param> 
+    ///<returns>true=转换成功</returns> 
+    public bool PPTConvertToPDF(string sourcePath, string targetPath)
+    {
+        bool result;
+        PowerPoint.PpSaveAsFileType targetFileType = PowerPoint.PpSaveAsFileType.ppSaveAsPDF;
+        object missing = Type.Missing;
+        PowerPoint.ApplicationClass application = null;
+        PowerPoint.Presentation persentation = null;
+        try
+        {
+            application = new PowerPoint.ApplicationClass();
+            persentation = application.Presentations.Open(sourcePath, MsoTriState.msoTrue, MsoTriState.msoFalse, MsoTriState.msoFalse); persentation.SaveAs(targetPath, targetFileType, Microsoft.Office.Core.MsoTriState.msoTrue);
+            result = true;
+        }
+        catch
+        {
+            result = false;
+        }
+        finally
+        {
+            if (persentation != null)
+            {
+                persentation.Close();
+                persentation = null;
+            }
+            if (application != null)
+            {
+                application.Quit();
+                application = null;
+            }
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+        return result;
+    }
+}
---
+++ TacroManagement/WebSites/TacroManagement/Library/TestLibrary.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class Library_TestLibrary : System.Web.UI.Page
+{
+    string saveName = "";
+    string uploadPath = "";
+    string savePath = "";
+
+    Pdf2Swf pdf2swf = new Pdf2Swf();
+    Office2Pdf office2pdf = new Office2Pdf();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    //上传文件到服务器
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        string fullFileName = File1.PostedFile.FileName;
+        //string fileType = File1.PostedFile.ContentType;//文件类型
+        int fileLength = File1.PostedFile.ContentLength;//文件大小
+        fullFileName = fullFileName.Substring(fullFileName.LastIndexOf("\\") + 1);// 取出文件名的路径（不包括文件的名称） 
+        /*
+               //上传文件到服务器 
+                string upload_file = Server.MapPath("./file/") + FileName;//取出服务器虚拟路径,存储上传文件 
+         */
+        //以日期建立文件夹
+        string path = "\\Library\\file\\" + DateTime.Now.ToString("yyyy-MM-dd") + "\\";
+        string ArticlePath = Server.MapPath("~") + path;
+        if (fileLength > 0 || !string.IsNullOrEmpty(fullFileName))
+        {
+            //建立主文件夹
+            if (!Directory.Exists(ArticlePath))
+            {
+                Directory.CreateDirectory(ArticlePath);
+            }
+
+            saveName = Path.GetFileName(fullFileName);
+            //以时间重新命名上传文件
+            string extension = Path.GetExtension(fullFileName).ToLower();
+
+            string fileName = DateTime.Now.ToString("HH-mm-ss") + extension;
+            ArticlePath += fileName;
+            //保存文件
+            this.File1.PostedFile.SaveAs(ArticlePath);
+
+            //pdf、office文件处理
+            string pdfpath = ArticlePath.Substring(0, ArticlePath.Length - extension.Length) + ".pdf";
+            string swfpath = ArticlePath.Substring(0, ArticlePath.Length - extension.Length) + ".swf";
+            if (extension == ".doc" || extension == ".docx")
+            {
+                office2pdf.DOCConvertToPDF(ArticlePath, pdfpath);
+                pdf2swf.PDFConvertToSWF(pdfpath, swfpath);
+            }
+
+            else if (extension == ".ppt" || extension == ".pptx")
+            {
+                office2pdf.PPTConvertToPDF(ArticlePath, pdfpath);
+                pdf2swf.PDFConvertToSWF(pdfpath, swfpath);
+            }
+            else if (extension == ".xls" || extension == ".xlsx")
+            {
+                office2pdf.XLSConvertToPDF(ArticlePath, pdfpath);
+                pdf2swf.PDFConvertToSWF(pdfpath, swfpath);
+            }
+            else if (extension == ".pdf")
+            {
+                pdf2swf.PDFConvertToSWF(ArticlePath, swfpath);
+            }
+
+            savePath = path.Substring(1, path.Length - 1) + fileName.Substring(0, fileName.Length - extension.Length) + ".swf";
+
+            uploadPath = path.Substring(1, path.Length - 1) + fileName;
+
+
+            //更新数据库
+
+
+
+        }
+    }
+
+
+}
